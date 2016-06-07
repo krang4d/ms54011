36961ae84312568aa9613a88d225d947de076906
P.G.Golovkin <jzi@inbox.ru>
2016-06-07 11:58:26 +0300

    fhird commit ms54011

--- ms54011.c
+++ ms54011.c
@@ -1,117 +1,122 @@
-/******************************
- * ms54011.c
- * Created: 03.06.2016 9:24:06
- * ATMega162-16pu
- ******************************/
+/*------------------------------
+--Board:--ms54011.c-------------
+--Created:-03.06.2016-9:24:06---
+--Controller:-ATMega162-16pu----
+--Author:-Golovkin-P-G----------
+--------------------------------*/
 #include <avr/io.h>
 #include <avr/interrupt.h>
+
+#define F_CPU 16000000UL
+
 #include <util/delay.h>
 
-#define F_CPU 16000000UL;
+unsigned char countInt0 = 0;
+unsigned char countInt1 = 0;
 
-unsigned int timer;
-unsigned short countInt0 = 0;
-unsigned short countInt1 = 0;
-
-//-----INTERROPT----//
+//--External-Interrupt-Functions-------------------------------------------------------//
 ISR(INT0_vect){
-	countInt0++;	//External Interrupt INT0 handler function
-	_delay_ms(1);
+	countInt0++;	
 }
 ISR(INT2_vect){
-	countInt1++;	//External Interrupt INT2 handler function
-	_delay_ms(1);
+	countInt1++;	
 }
 ISR (TIMER1_OVF_vect){
-	;
+	PORTB ^= (1<<PB0);
 }
-//------------------//
+//--Main-Functions---------------------------------------------------------------------//
 void setupInterrupt(void){
-	GICR = (1<<INT2) | (1<<INT0);	/*GICR=0b11000000;	General Interrupt Control Register - GICR Enable INT1, INT0*/
-	MCUCR = (1<<ISC11) | 1<<(ISC01);/*MCUCR=0b00001010;	The falling edge of INT1, INT2 generates an interrupt request*/
-	sei();							/*SREG=(1<<7); Status Register Global Interrupt Enable*/
+//--GICR=0b11000000---General Interrupt Control Register - GICR Enable INT1, INT0------//
+	GICR = (1<<INT2) | (1<<INT0);
+//--MCUCR=0b00001010--The falling edge of INT1, INT2 generates an interrupt request----//
+	MCUCR = (1<<ISC11) | 1<<(ISC01);
+//--SREG=(1<<7)-------Status Register Global Interrupt Enable--------------------------//
+	sei();
 }
 void setupTimer1(void){
-	//With 16 MHz clock and 65536 times counting T/C1 overflow interrupt
-	// will occur every:
-	//   1<<CS10                  4096 mkS  (no prescale Fclk/1)
-	//   1<<CS11                  32.768 mS (Fclk/8)
-	//  (1<<CS11)|(1<<CS10)       262.144 mS (Fclk/64)
-	
-	TCCR1B = (1<<CS12); //1048.576 mS (Fclk/256)
+	TCCR1B = (1<<CS12);			//1048.576 mS (F_CPU/256)
 	TCNT1 = 65536-62439;		//1s
-	/* Enable timer 1 overflow interrupt. */
+//--Enable timer 1 overflow interrupt-----------------------------------------------//
 	TIMSK = (1<<TOIE1);
 }
 
 void setupPorts(void){
-	SFIOR |= ~(1<<PUD); // Pull-up enabled
+	SFIOR |= ~(1<<PUD); //Pull-up enabled
+//---------Port A Initialization----------------------------------------------------//
+	DDRA = 0x00;    //to read,
+	PORTA = 0xFF;	//pull-up resistor
 	
-	DDRA = 0x00;	//Port A Initialization to read,
-	PORTA = 0xFF;	//pull-up resistor ON.
+//---------Port B Initialization----------------------------------------------------//	
+	DDRB = (1<<PB0); //PB0-TEST TIMER,
+	PORTB = 0xFF;	 //pull-up resistor
 	
-	DDRB = 0x00;	//Port B Initialization to read,
-	PORTB = 0xFF;	//pull-up resistor ON.
+//---------Port C Initialization----------------------------------------------------//	
+	DDRC = 0x00;	//to read,
+	PORTC = 0xFF;	//pull-up resistor
 	
-	DDRC = 0xFF;	//Port C Initialization to read,
-	PORTC = 0xFF;	//pull-up resistor ON.
+//---------Port D Initialization----------------------------------------------------//
+//------------RX---------TX--------INT0------GTS_MK---------------------------------//
+	DDRD = (0<<PD0) | (1<<PD1) | (0<<PD2) | (1<<PD3) |
+//----------Start_In--Direct_In--Start_Out--Direct_Out------------------------------//
+			(0<<PD4) | (0<<PD5) | (1<<PD6) | (1<<PD7);
+			
+	PORTD = (0<<PD3) | (1<<PD4) | (1<<PD5) | (0<<PD6) | (0<<PD7);
 	
-	//DDRC = (1<<INT1) | (1<<PD4) | (1<<PD5) | (0<<PD);    //Port D Initialization  
-	DDRD = 0xFF;
-	PORTD = 0xFF;
-	
-	DDRE = 0xFF;
-	PORTE = 0xff;
+//---------Port E Initialization----------------------------------------------------//
+//----------INT2-----DTO_to_BSP-----------------------------------------------------//
+	DDRE = (0<<PE0) | (1<<PE1);
+//----------Pull-up----DTO_to_BSP---------------------------------------------------//
+	PORTE = (1<<PE0) | (0<<PE1);
 }
-//-------------------------------------------------//
+//----------------------------------------------------------------------------------//
 void setDirect(void){
 	if ( PORTD & (1<<PD5) )
-		PORTD |= (1<<PD7); // Set true to  bit1 in port C
+		PORTD |= (1<<PD7); //Forward
 	else
-		PORTD &= ~(1<<PD7); // Set false to  bit1 in port C
+		PORTD &= ~(1<<PD7); //Backward
 }
-//--------------------------------------------------//
+//----------------------------------------------------------------------------------//
 void start(void){
-	setDirect();
-	if ( PORTD & (1<<PD4) )
-		PORTD |= (1<<PD6); // Set true to  bit1 in port C
+	if ( PORTD & (1<<PD4) ){
+		setDirect();
+		PORTD |= (1<<PD6); //Start
+	}
 	else
-		PORTD &= ~(1<<PD6); // Set false to  bit1 in port C
+		PORTD &= ~(1<<PD6); //Stop
 }
-//-------------------------------------------------//
+//---------------------------------------------------------------------------------//
 void setGTS(void){
 	if ( PORTB & (1<<PB7)){
 		if ( (PORTB & (1<<PB1)) || (PORTB & (1<<PB4)) || (PORTB & (1<<PB5)) || (PORTB & (1<<PB6)) )
 			PORTD |= (1<<PD3);
 	}
 }
-//-------------------------------------------------//
+//---------------------------------------------------------------------------------//
 void setDTO(void){
 	if ( (countInt0 > 0) && (countInt1 > 0)){
-//-------------DTO-to-PC----------------//
-		PORTC |= (1<<PC2); 
+		PORTC |= (1<<PC2); //to-PC
+		PORTE |= (1<<PE1); //to-BSP
 		_delay_ms(10);
 		PORTC &= ~(1<<PC2);
-//-------------DTO-to-BSP----------------//
-		PORTE |= (1<<PE1);
-		_delay_ms(10);
 		PORTE &= ~(1<<PE1);
-//--------------------------------------//
 		_delay_ms(1000);
 		countInt0 = 0;
 		countInt1 = 0;
 	}
-	else (PORTD &= ~(1<<PD2));
+	else{
+		PORTC &= ~(1<<PC2);
+		PORTE &= ~(1<<PE1);
+	}
 }
-//---------------------------------------------------//
+//---------------------------------------------------------------------------------//
 int main(void){
 	setupInterrupt();
 	setupPorts();
 	setupTimer1();
 	
     while(1){
-		start();	
-		setGTS();	
-		setDTO();	
+		start();
+		setGTS();
+		setDTO();
     }
 }
