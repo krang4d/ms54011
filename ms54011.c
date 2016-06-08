3b4e35e76d70db6a31c3bdd84b5e00107732bb24
P.G.Golovkin <jzi@inbox.ru>
2016-06-08 17:24:21 +0300

    fifth commit ms54011

--- ms54011.c
+++ ms54011.c
@@ -8,50 +8,55 @@
 #include <avr/interrupt.h>
 
 #define F_CPU 8000000UL
-
 #include <util/delay.h>
 
 unsigned char countInt0 = 0;
 unsigned char countInt1 = 0;
 
-//--External-Interrupt-Functions-------------------------------------------------------//
+//--External-Interrupt-Procedure-------------------------------------------------------//
 ISR(INT0_vect){
-	countInt0++;	
+	countInt0++;
+	PORTB ^= (1<<PB0);	
 }
 ISR(INT2_vect){
-	countInt1++;	
+	countInt1++;
+	PORTB ^= (1<<PB0);	
 }
 ISR(TIMER1_OVF_vect){
 	PORTB ^= (1<<PB0);
 }
 //--Main-Functions---------------------------------------------------------------------//
 void setupInterrupt(void){
-//--GICR=0b11000000---General Interrupt Control Register - GICR Enable INT1, INT0------//
+	cli();
+//--MCUCR=0b00000011--The rising edge of INT0, INT2 generates an interrupt request-----//
+	MCUCR = (1<<ISC00) | 1<<(ISC01);
+	EMCUCR = (1<<ISC2);
+//--GICR=0b11000000---General Interrupt Control Register - GICR Enable INT0, INT2------//
 	GICR = (1<<INT2) | (1<<INT0);
-//--MCUCR=0b00001010--The falling edge of INT1, INT2 generates an interrupt request----//
-	MCUCR = (1<<ISC11) | 1<<(ISC01);
 //--SREG=(1<<7)-------Status Register Global Interrupt Enable--------------------------//
 	sei();
 }
 void setupTimer1(void){
-	TCCR1B = (1<<CS12);			//2048 mS (F_CPU/256)
-	TCNT1 = 32768;			//1024 mS
+	cli();
+	TCCR1B = (1<<CS12) | (1<<CS10);	//2048 mS (F_CPU/1028)
+	TCNT1 = 32768;				//1024 mS
 	TIMSK = (1<<TOIE1);			//Enable timer 1 overflow interrupt
 	sei();
 }
 void setupPorts(void){
+	cli();
 	SFIOR |= ~(1<<PUD); //Pull-up enabled
 //---------Port A Initialization----------------------------------------------------//
 	DDRA = 0x00;    //to read,
 	PORTA = 0xFF;	//pull-up resistor
 	
 //---------Port B Initialization----------------------------------------------------//	
-	DDRB = (1<<PB0); //PB0-TEST TIMER,
-	PORTB = 0xFF;	 //pull-up resistor
+	DDRB = (1<<PB0); //PB0-TESTER
+	PORTB = ~(1<<PB0);	 //pull-up resistor
 	
 //---------Port C Initialization----------------------------------------------------//	
-	DDRC = 0x00;	//to read,
-	PORTC = 0xFF;	//pull-up resistor
+	DDRC = (1<<PC2);
+	PORTC = ~(1<<PC2);	//pull-up resistor
 	
 //---------Port D Initialization----------------------------------------------------//
 //------------RX---------TX--------INT0------GTS_MK---------------------------------//
@@ -59,24 +64,25 @@ void setupPorts(void){
 //----------Start_In--Direct_In--Start_Out--Direct_Out------------------------------//
 			(0<<PD4) | (0<<PD5) | (1<<PD6) | (1<<PD7);
 			
-	PORTD = (0<<PD3) | (1<<PD4) | (1<<PD5) | (0<<PD6) | (0<<PD7);
+	PORTD = (1<<PD4) | (1<<PD5); //pull-up resistor
 	
 //---------Port E Initialization----------------------------------------------------//
-//----------INT2-----DTO_to_BSP-----------------------------------------------------//
-	DDRE = (0<<PE0) | (1<<PE1);
+//--------DTO_to_BSP-----------------------------------------------------//
+	DDRE = (1<<PE1);
 //----------Pull-up----DTO_to_BSP---------------------------------------------------//
 	PORTE = (1<<PE0) | (0<<PE1);
+	sei();
 }
 //----------------------------------------------------------------------------------//
 void setDirect(void){
-	if ( PORTD & (1<<PD5) )
+	if ( PIND & (1<<PD5) )
 		PORTD |= (1<<PD7); //Forward
 	else
 		PORTD &= ~(1<<PD7); //Backward
 }
 //----------------------------------------------------------------------------------//
 void start(void){
-	if ( PORTD & (1<<PD4) ){
+	if ( PIND & (1<<PD4) ){
 		setDirect();
 		PORTD |= (1<<PD6); //Start
 	}
@@ -85,21 +91,23 @@ void start(void){
 }
 //---------------------------------------------------------------------------------//
 void setGTS(void){
-	if ( PORTB & (1<<PB7)){
-//------------------------G1--------------------G2--------------------G3--------------------G4---//
-		if ( (PORTB & (1<<PB1)) || (PORTB & (1<<PB4)) || (PORTB & (1<<PB5)) || (PORTB & (1<<PB6)) )
+	if ( PINB & (1<<PB7) ){
+//----------------G1-------------------G2-------------------G3-------------------G4----------//
+		if ( (PINB & (1<<PB1)) || (PINB & (1<<PB4)) || (PINB & (1<<PB5)) || (PINB & (1<<PB6)) )
 			PORTD |= (1<<PD3);
+		else PORTD &= ~(1<<PD3);
 	}
+	else PORTD &= ~(1<<PD3);
 }
 //---------------------------------------------------------------------------------//
 void setDTO(void){
 	if ( (countInt0 > 0) && (countInt1 > 0)){
 		PORTC |= (1<<PC2); //to-PC
 		PORTE |= (1<<PE1); //to-BSP
-		_delay_ms(10);
+		_delay_ms(1000);
 		PORTC &= ~(1<<PC2);
 		PORTE &= ~(1<<PE1);
-		_delay_ms(1000);
+		//_delay_ms(1000);
 		countInt0 = 0;
 		countInt1 = 0;
 	}
