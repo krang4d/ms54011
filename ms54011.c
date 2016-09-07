2db8bdcef858c6e5d45eb90a7059b64129590c11
P.G.Golovkin <jzi@inbox.ru>
2016-09-07 14:45:00 +0300

    edited Stroscode by me

--- ms54011.c
+++ ms54011.c
@@ -1,10 +1,10 @@
-﻿/*------------------------------
---Board:--ms54011.c-------------
---Created:-03.06.2296-9:24:06---
---Controller:-ATMega162-16pu----
---Author:-Golovkin-P-G---------- и стросс в части уарта
---------------------------------*/
-#define F_CPU 2000000UL // 2 МГЦ БЛЕАТЬ!!!! Потому что фьюз деления на 8 стоит
+﻿/*-----------------------------------
+--Board:--ms54011.c------------------
+--Created:-03.06.2296-9:24:06--------
+--Controller:-ATMega162-16pu---------
+--Author:-Golovkin-P-G---IGNATOV-A-S-
+------------------------------------*/
+#define F_CPU 2000000UL // 2 МГЦ !!! Потому что фьюз деления на 8 стоит Error!!!
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -13,15 +13,23 @@
 #define BAUD 9600
 #define MYUBRR F_CPU/16/BAUD-1
 
+//--bit definitions in transmitted byte--------------------------------------------------//
+#define NU		0	// Not used
+#define GTS1	1	// ГТС1
+#define DTO1	2	// ДТО1
+#define DTO2	3	// ДТО2
+#define GTS2	4	// ГТС2
+#define GTS3	5	// ГТС3
+#define GTS4	6	// ГТС4
+#define STS		7	// СТС (get power)
+
 unsigned char countInt0 = 0;
 unsigned char countInt1 = 0;
 
 unsigned char wasDTO1 = 0;
 unsigned char wasDTO2 = 0;
 
-unsigned char USART_Receive(void)
-{
-	
+unsigned char USART_Receive(void){
 	/* Wait for data to be received */
 	while ( !(UCSR0A & (1<<RXC0)) )
 	;
@@ -46,11 +54,9 @@ ISR(TIMER1_OVF_vect){
 	
 	PORTB ^= (1<<PB0);
 }
-ISR(USART0_RXC_vect)
-{
+ISR(USART0_RXC_vect){
 	c = USART_Receive();
-			switch (c)
-			{
+			switch (c){
 				case '0':
 				PORTD &= ~(1<<PD7); //Stop
 				PORTD &= ~(1<<PD6); //Release
@@ -103,13 +109,13 @@ void setupPorts(void){
 //---------Port D Initialization----------------------------------------------------//
 //------------RX---------TX--------INT0------GTS_MK---------------------------------//
 	DDRD = (0<<PD0) | (1<<PD1) | (0<<PD2) | (1<<PD3) |
-//----------Start_In--Direct_In--Start_Out--Direct_Out------------------------------//
+//----------Start_In--Direct_In--Direct_Out--Start_Out------------------------------//
 			(0<<PD4) | (0<<PD5) | (1<<PD6) | (1<<PD7);
 			
 	PORTD = (1<<PD4) | (1<<PD5); //pull-up resistor
 	
 //---------Port E Initialization----------------------------------------------------//
-//--------DTO_to_BSP-----------------------------------------------------//
+//--------DTO_to_BSP----------------------------------------------------------------//
 	DDRE = (1<<PE1);
 //----------Pull-up----DTO_to_BSP---------------------------------------------------//
 	PORTE = (1<<PE0) | (0<<PE1);
@@ -134,11 +140,10 @@ void start(void){
 		PORTD |= (1<<PD7); //Start
 	}
 }
-
 //---------------------------------------------------------------------------------//
 void setGTS(void){
 	if ( PINB & (1<<PB7) ){
-//----------------G1-------------------G2-------------------G3-------------------G4----------//
+//----------------GTS1-----------------GTS2-----------------GTS3----------------GTS4----------//
 		if ( (PINB & (1<<PB1)) || (PINB & (1<<PB4)) || (PINB & (1<<PB5)) || (PINB & (1<<PB6)) )
 			PORTD |= (1<<PD3);
 		else PORTD &= ~(1<<PD3);
@@ -162,10 +167,8 @@ void setDTO(void){
 		PORTE &= ~(1<<PE1);
 	}
 }
-//govnocode start
-
-void UART_Init(unsigned int ubrr)
-{
+//stroscode start
+void UART_Init(unsigned int ubrr){
 	/* Set baud rate */
 	UBRR0H = (unsigned char)(ubrr>>8);
 	UBRR0L = (unsigned char)ubrr;
@@ -174,69 +177,84 @@ void UART_Init(unsigned int ubrr)
 	/* Set frame format: 8data, 1stop bit */
 	UCSR0C = (1<<URSEL0)|(0<<USBS0)|(3<<UCSZ00);
 }
-
-void UART_Transmit(unsigned char data)
-{
+void UART_Transmit(unsigned char data){
 	/* Wait for empty transmit buffer */
 	while ( !( UCSR0A & (1<<UDRE0)) )
 	;
 	/* Put data into buffer, sends the data */
 	UDR0 = data;
 }
-
-
-//govnocode end
-
-
-
-
+void Frame_Transmit(){
+	unsigned char ByteToSend = 0;
+	if ((PINB & (1 << PB1)){			//Set GTS1 bit
+		ByteToSend |= (1<<GTS1);	
+	else ByteToSend &= ~(1<<GTS1);	
+	}
+	if ((PINA & (1 << PA0) & wasDTO1){	//Set DTO1 bit
+		ByteToSend |= (1<<DTO1);		
+		else ByteToSend &= ~(1<<DTO1);
+	}
+	if ((PINA & (1 << PA1) & wasDTO2){	//Set DTO2 bit
+		ByteToSend |= (1<<DTO2);		
+		else ByteToSend &= ~(1<<DTO2);
+	}
+	if ((PINB & (1 << PB4)){			//Set GTS2 bit
+		ByteToSend |= (1<<GTS2);
+		else ByteToSend &= ~(1<<GTS2);
+	}
+	if ((PINB & (1 << PB5)){			//Set GTS3 bit
+		ByteToSend |= (1<<GTS3);
+		else ByteToSend &= ~(1<<GTS3);
+	}
+	if ((PINB & (1 << PB6)){			//Set GTS4 bit
+		ByteToSend |= (1<<GTS4);
+		else ByteToSend &= ~(1<<GTS4);
+	}
+	if ((PINB & (1 << PB7)){			//Set STS bit
+		ByteToSend |= (1<<STS);
+		else ByteToSend &= ~(1<<STS);
+	}
+	UART_Transmit(ByteToSend);
+	wasDTO1 = 0;
+	wasDTO2 = 0;
+	// Old school Srtos hard code :)
+	//ByteToSend =
+			//((PINB & (1 << PINB1)) >> 0)| //Получается такой байт:
+			//((PINB & (1 << PINB4)) >> 0)| //(П)(Г4)(Г3)(Г2)(ДТО2)(ДТО1)(Г1)(не задействован)
+			//((PINB & (1 << PINB5)) >> 0)|
+			//((PINB & (1 << PINB6)) >> 0)|
+			//((PINB & (1 << PINB7)) >> 0)|
+			//((PINA & (1 << PINA0)) << 2)|
+			//((PINA & (1 << PINA1)) << 2)|
+			//wasDTO1|wasDTO2;
+			//UART_Transmit(ByteToSend);
+			//wasDTO1 = 0;
+			//wasDTO2 = 0;
+}
+//stroscode end
 //---------------------------------------------------------------------------------//
 int main(void){
-	//govnocode start
+	//stroscode start
 	UART_Init(MYUBRR);
 	int divider = 0;
-
-	unsigned char ByteToSend = 0;
-	//govnocode end
+	
+	//stroscode end
 	setupInterrupt();
 	setupPorts();
 	setupTimer1();
 	
-
-		
     while(1){
 		//start();
-		
 		setGTS();
 		setDTO();
-		//govnocode start
-		
-
-		
-		if (divider == 0)
-		{
-			ByteToSend =
-			((PINB & (1 << PINB1)) >> 0)| //Получается такой байт:
-			((PINB & (1 << PINB4)) >> 0)| //(П)(Г4)(Г3)(Г2)(ДТО2)(ДТО1)(Г1)(не задействован)
-			((PINB & (1 << PINB5)) >> 0)|
-			((PINB & (1 << PINB6)) >> 0)|
-			((PINB & (1 << PINB7)) >> 0)|
-			((PINA & (1 << PINA0)) << 2)|
-			((PINA & (1 << PINA1)) << 2)|
-			wasDTO1|wasDTO2;
-			UART_Transmit(ByteToSend);
-			wasDTO1 = 0;
-			wasDTO2 = 0;
+		//stroscode start
+		if (divider == 0){
+			Frame_Transmit();
 		}
 		divider++;
-		if (divider >= 30000)
-		{
+		if (divider >= 30000){
 			divider = 0;
 		}
-		
-		
-		//govnocode end
-		
-		
+		//stroscode end
     }
 }
